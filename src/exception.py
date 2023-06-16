532bad397c5911c063aa88206a653268590f70b8
Pradeep Shete <pradeep.shete2394@gmail.com>
2023-06-16 20:39:09 +0530

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -8,7 +8,7 @@ def error_message_detail(error,error_details:sys):
     file_name,exc_tb.tb_lineno,str(error))
 
 
-class CustomeException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
